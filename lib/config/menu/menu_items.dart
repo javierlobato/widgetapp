4774cf4c9d420809468596798eade534c68ae31b
Javier Lobato <davidlobato99@gmail.com>
2024-01-26 22:33:18 -0500

    Create menu_items

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MenuItems {
+  final String title;
+  final String subTitle;
+  final String url;
+  final IconData icon;
+
+  const MenuItems(
+      {required this.title,
+      required this.subTitle,
+      required this.url,
+      required this.icon});
+}
+
+const appMenuItems = <MenuItems>[
+
+  MenuItems(
+    title: 'Botones', 
+    subTitle: 'Varios Botones en Flutter', 
+    url: '/buttons', 
+    icon: Icons.smart_button_outlined,
+    ),
+
+    MenuItems(
+    title: 'Tarjetas', 
+    subTitle: 'Un contenedor estilizado', 
+    url: '/card', 
+    icon: Icons.credit_card ,
+    )
+];
