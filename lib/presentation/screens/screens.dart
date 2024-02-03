8a442862aac5ec54f2ac98c66aa314492407c105
Javier Lobato <davidlobato99@gmail.com>
2024-02-02 23:34:01 -0500

    correction of errors in import and barrel file

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,11 +1,13 @@
 
 
-export 'package:widgets_app/presentation/screens/animated/animated_screen.dart';
-export 'package:widgets_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
-export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
-export 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
-export 'package:widgets_app/presentation/screens/home/home_screen.dart';
-export 'package:widgets_app/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
-export 'package:widgets_app/presentation/screens/progress/progress_screen.dart';
-export 'package:widgets_app/presentation/screens/snackbar/snackbar_screen.dart';
-export 'package:widgets_app/presentation/screens/ui_controls/ui_controls_screen.dart';
+
+export 'package:widgetapp/presentation/screens/buttons/buttons_screen.dart';
+export 'package:widgetapp/presentation/screens/app_tutorial/app_tutorial_screen.dart';
+export 'package:widgetapp/presentation/screens/animated/animated_screen.dart';
+export 'package:widgetapp/presentation/screens/cards/cards_screen.dart';
+export 'package:widgetapp/presentation/screens/home/home_screen.dart';
+export 'package:widgetapp/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgetapp/presentation/screens/progress/progress_screen.dart';
+export 'package:widgetapp/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgetapp/presentation/screens/ui_controls/ui_controls_screen.dart';
+
