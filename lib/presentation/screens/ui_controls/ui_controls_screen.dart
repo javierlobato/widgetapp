0051cf9cc717d659e03bb9d41043a842fe64c6ad
Javier Lobato <davidlobato99@gmail.com>
2024-02-15 10:57:32 -0500

    ExpansionTile and CheckboxTile finished

--- lib/presentation/screens/ui_controls/ui_controls_screen.dart
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -29,6 +29,10 @@ class _UiControlsViewState extends State<_UiControlsView> {
 
   Transportation selectedTransportation = Transportation.car;
 
+  bool wantsBreakfast = false;
+  bool wantsLunch = false;
+  bool wantsDinner = false;
+
  
 
   @override
@@ -45,8 +49,13 @@ class _UiControlsViewState extends State<_UiControlsView> {
           }),
         ),
         
-        
-        RadioListTile(
+        ExpansionTile(
+          title: const Text('Vehiculo de Transporte'),
+          subtitle: Text('$selectedTransportation'),
+          
+
+          children: [
+            RadioListTile(
             title: const Text('Car'),
             subtitle: const Text('Travel in car'),
             value: Transportation.car, 
@@ -85,6 +94,35 @@ class _UiControlsViewState extends State<_UiControlsView> {
               selectedTransportation = Transportation.submarine;
             }),
         ),
+          ],
+        ),
+
+        CheckboxListTile(
+          title: const Text('¿Breakfast?'),
+          value: wantsBreakfast, 
+          onChanged: (value) => setState(() {
+            wantsBreakfast = !wantsBreakfast;
+          })
+        ),
+
+        CheckboxListTile(
+          title: const Text('¿Lunch?'),
+          value: wantsLunch, 
+          onChanged: (value) => setState(() {
+            wantsLunch = !wantsLunch;
+          })
+        ),
+
+        CheckboxListTile(
+          title: const Text('¿Dinner?'),
+          value: wantsDinner, 
+          onChanged: (value) => setState(() {
+            wantsDinner = !wantsDinner;
+          })
+        ),
+
+        
+        
 
       ],
     );
